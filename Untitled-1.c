63410fa6ae57889fa75270d2170b554fa57a3f42
Preet <57300194+amnssi@users.noreply.github.com>
2024-08-09 10:58:07 -0700

    Add files via upload

---
+++ Untitled-1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+void main ()
+{
+int number;
+printf("Enter a value")
+scanf("%d", &num);
+
+if (num % 2 == 0) {
+    printf("%d is an even number");
+    else 
+    printf("%d is an odd number");
+}
+    return 0;
+}
